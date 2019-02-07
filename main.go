7b2787a27828c1ef6cca48cb5916642a928b3743
Jacob Payne <paynejacob21@gmail.com>
2019-02-07 15:34:55 -0500

    foormatting

--- main.go
+++ main.go
@@ -51,8 +51,8 @@ type Message struct {
 }
 
 func NewEventMessage(event *types.Event) *Message {
-	message := &Message{ThemeColor: getColor(event), Text: getMessageStatus(event), Channel: channel} // TODO support channel from annotation
-	message.Sections = append(message.Sections, Section{fmt.Sprintf("%s: %s", event.Check.Name, event.Check.Output)})
+	message := &Message{ThemeColor: getColor(event), Text: fmt.Sprintf("(%s) %s: %s", getMessageStatus(event), event.Check.Name, event.Check.Output), Channel: channel} // TODO support channel from annotation
+	message.Sections = append(message.Sections, Section{"Nothing for now"})
 	message.PotentialAction = append(message.PotentialAction, PotentialAction{Type: "OpenUri", Name: "View in Sensu"})
 	message.PotentialAction[0].Targets = append(message.PotentialAction[0].Targets, Target{"default", getLink(event)})
 
@@ -70,7 +70,7 @@ func getLink(event *types.Event) string {
 		return ""
 	}
 
-	if eventPath, err = url.Parse(fmt.Sprintf("%s/%s", url.PathEscape(event.Entity.Name), url.PathEscape(event.Check.Name))); err != nil {
+	if eventPath, err = url.Parse(fmt.Sprintf("/default/events/%s/%s", url.PathEscape(event.Entity.Name), url.PathEscape(event.Check.Name))); err != nil {
 		return ""
 	}
 
