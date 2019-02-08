3216ddc205537d935a00d146f070d4d09c62573e
Jacob Payne <paynejacob21@gmail.com>
2019-02-08 09:29:06 -0500

    more message formatting

--- main.go
+++ main.go
@@ -52,7 +52,9 @@ type Message struct {
 
 func NewEventMessage(event *types.Event) *Message {
 	message := &Message{ThemeColor: getColor(event), Text: getMessageStatus(event), Channel: channel} // TODO support channel from annotation
-	message.Sections = append(message.Sections, Section{fmt.Sprintf("%s: %s", event.Check.Name, event.Check.Output)})
+	message.Sections = append(message.Sections, Section{fmt.Sprintf(event.Entity.System.Hostname)})
+	message.Sections = append(message.Sections, Section{fmt.Sprintf(event.Check.Name)})
+	message.Sections = append(message.Sections, Section{fmt.Sprintf(event.Check.Output)})
 	message.PotentialAction = append(message.PotentialAction, PotentialAction{Type: "OpenUri", Name: "View in Sensu"})
 	message.PotentialAction[0].Targets = append(message.PotentialAction[0].Targets, Target{"default", getLink(event)})
 
